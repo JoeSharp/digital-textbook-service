149fdef9d082a0cdb82dbce9a01ea572a2831392
GCHQ 11 <gchq.developer.11@gmail.com>
2020-03-23 14:10:48 +0000

    pushed config out to file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
+import * as logger from "winston";
 import { MongoClient } from "mongodb";
+
+import * as config from "../config.json";
+
+logger.configure({
+  level: "debug",
+  transports: [new logger.transports.Console()]
+});
+
+logger.info(`Running with config: ${JSON.stringify(config, null, 2)}`);
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -8,16 +18,7 @@ app.use(bodyParser.json());
 
 const port = 8080; // default port to listen
 
-const DB_USER = "admin";
-const DB_PASSWORD = "JP1pTsVh";
-const DB_URL =
-  "csdigitaltextbook-bec0y.mongodb.net/test?retryWrites=true&w=majority";
-const DB_NAME = "teach-code";
-const COLLECTION_COURSES = "courses";
-const UI_ORIGIN = "http://localhost:3000";
 const CORS_HEADER = "Access-Control-Allow-Origin";
-
-const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_URL}`;
 
 interface Course {
   name: string;
@@ -25,12 +26,17 @@ interface Course {
 
 // define a route handler for the default home page
 app.get("/courses", async (req, res) => {
-  const client = new MongoClient(uri, { useNewUrlParser: true });
+  const client = new MongoClient(config.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
   try {
     await client.connect();
 
-    const collection = client.db(DB_NAME).collection(COLLECTION_COURSES);
+    const collection = client
+      .db(config.DB_NAME)
+      .collection(config.DB_COLLECTION_COURSES);
     // perform actions on the collection object
     const courses: Course[] = [];
     const cursor = collection.find();
@@ -39,9 +45,10 @@ app.get("/courses", async (req, res) => {
       courses.push(course as Course);
     }
 
-    res.setHeader(CORS_HEADER, UI_ORIGIN);
+    res.setHeader(CORS_HEADER, config.UI_ORIGIN);
     res.send(courses);
   } catch (err) {
+    logger.error(err);
     res.sendStatus(500);
   }
 
@@ -51,6 +58,5 @@ app.get("/courses", async (req, res) => {
 
 // start the Express server
 app.listen(port, () => {
-  // tslint:disable-next-line:no-console
-  console.log(`server started at http://localhost:${port}`);
+  logger.info(`server started at http://localhost:${port}`);
 });
---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export interface Config {
+  DB_URL: string;
+  DB_NAME: string;
+  DB_COLLECTION_COURSES: string;
+  UI_ORIGIN: string;
+}
